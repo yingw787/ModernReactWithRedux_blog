13ebf07b61f1dd92114fb20850bc3776a275fc86
Ying Wang <ywang@xometry.com>
2017-01-19 09:19:17 -0500

    Pushing changed files to remote for transfer to personal computer.

---
+++ src/components/posts_index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                List of blog posts.
+            </div>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+const Greeting = () => {
+    return <div>Hey there!</div>
+}
+
+export default (
+    <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex} />
+        <Route path="posts/new" component={PostsNew} />
+    </Route>
+);
