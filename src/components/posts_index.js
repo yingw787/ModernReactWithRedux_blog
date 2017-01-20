7056719b041de0eb8ae411cfb374417384769255
Ying Wang <yingw787@gmail.com>
2017-01-19 19:44:50 -0500

    Finished application.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,6 +8,18 @@ class PostsIndex extends Component {
     componentWillMount() {
         this.props.fetchPosts();
     }
+    renderPosts() {
+        return this.props.posts.map((post) => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={"posts/" + post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>
+                </li>
+            )
+        })
+    }
     render() {
         return (
             <div>
@@ -16,14 +28,17 @@ class PostsIndex extends Component {
                         Add a Post
                     </Link>
                 </div>
-                List of blog posts.
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ fetchPosts }, dispatch);
+function mapStateToProps(state) {
+    return { posts: state.posts.all };
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const Greeting = () => {
     return <div>Hey there!</div>
@@ -13,5 +14,6 @@ export default (
     <Route path="/" component={App}>
         <IndexRoute component={PostsIndex} />
         <Route path="posts/new" component={PostsNew} />
+        <Route path="posts/:id" component={PostsShow} />
     </Route>
 );
